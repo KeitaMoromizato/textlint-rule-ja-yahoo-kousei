c6d1ff2fbe61356a3b000455bf7efc929bb846c7
Keita Moromizato <keita_moromizato@relationsgroup.co.jp>
2015-11-21 22:08:01 +0900

    feature: filter

--- test/yahoo-kousei-test.js
+++ test/yahoo-kousei-test.js
@@ -1,28 +1,47 @@
 "use strict";
 
 import assert from 'power-assert';
-import {textlint} from 'textlint';
+import {TextLintCore} from 'textlint';
 import rule from '../src/ja-yahoo-kousei.js';
 
 describe('yahoo-kousei', function() {
 
-  beforeEach(() => {
+  context('when use sample text', () => {
+    const textlint = new TextLintCore();
+
     textlint.setupRules({
       'ja-yahoo-kousei': rule
     });
-  });
 
-  afterEach(() => {
-    textlint.resetRules();
-  });
-
-  context('when use sample text', () => {
-    it('should report error', () => {
+    it('should report error', (done) => {
       // sample text from
       // http://developer.yahoo.co.jp/webapi/jlp/kousei/v1/kousei.html
       textlint.lintMarkdown('遙か彼方に小形飛行機が見える。').then(result => {
         assert(result.messages.length === 3);
-      });
+      }).then(done, done);
+    });
+  });
+
+  context('when use filter config', () => {
+    const textlint = new TextLintCore();
+
+    textlint.setupRules({
+      'ja-yahoo-kousei': rule
+    }, {
+      'ja-yahoo-kousei': {
+        'ignores': {
+          '用字':['彼方']
+        }
+      }
+    });
+
+    it('should report filtered error', (done) => {
+
+      // sample text from
+      // http://developer.yahoo.co.jp/webapi/jlp/kousei/v1/kousei.html
+      textlint.lintMarkdown('遙か彼方に小形飛行機が見える。').then(result => {
+        assert(result.messages.length === 2);
+      }).then(done, done);
     });
   });
 });
